f0b6d3fd7077042f2b79ad97bb2d476f6289a3c9
Tinky <48660658+Tinky124@users.noreply.github.com>
2020-06-05 23:47:00 +0200

    Minor Changes

--- client/main.lua
+++ client/main.lua
@@ -1,11 +1,10 @@
-ESX          = nil
-local IsDead = false
-local IsAnimated = false
+ESX = nil
+local IsDead, IsAnimated = false, false
 
-Citizen.CreateThread(function()
+CreateThread(function()
 	while ESX == nil do
 		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(0)
+		Wait(0)
 	end
 end)
 
@@ -19,16 +18,11 @@ AddEventHandler('esx_basicneeds:healPlayer', function()
 	-- restore hunger & thirst
 	TriggerEvent('esx_status:set', 'hunger', 1000000)
 	TriggerEvent('esx_status:set', 'thirst', 1000000)
-
 	-- restore hp
 	local playerPed = PlayerPedId()
 	SetEntityHealth(playerPed, GetEntityMaxHealth(playerPed))
 end)
-
-AddEventHandler('esx:onPlayerDeath', function()
-	IsDead = true
-end)
-
+AddEventHandler('esx:onPlayerDeath', function() IsDead = true end)
 AddEventHandler('esx:onPlayerSpawn', function(spawn)
 	if IsDead then
 		TriggerEvent('esx_basicneeds:resetStatus')
@@ -51,9 +45,9 @@ AddEventHandler('esx_status:loaded', function(status)
 		status.remove(75)
 	end)
 
-	Citizen.CreateThread(function()
+	CreateThread(function()
 		while true do
-			Citizen.Wait(1000)
+			Wait(1000)
 
 			local playerPed  = PlayerPedId()
 			local prevHealth = GetEntityHealth(playerPed)
@@ -86,9 +80,7 @@ AddEventHandler('esx_status:loaded', function(status)
 	end)
 end)
 
-AddEventHandler('esx_basicneeds:isEating', function(cb)
-	cb(IsAnimated)
-end)
+AddEventHandler('esx_basicneeds:isEating', function(cb) cb(IsAnimated) end)
 
 RegisterNetEvent('esx_basicneeds:onEat')
 AddEventHandler('esx_basicneeds:onEat', function(prop_name)
@@ -96,23 +88,20 @@ AddEventHandler('esx_basicneeds:onEat', function(prop_name)
 		prop_name = prop_name or 'prop_cs_burger_01'
 		IsAnimated = true
 
-		Citizen.CreateThread(function()
+		CreateThread(function()
 			local playerPed = PlayerPedId()
 			local x,y,z = table.unpack(GetEntityCoords(playerPed))
 			local prop = CreateObject(GetHashKey(prop_name), x, y, z + 0.2, true, true, true)
 			local boneIndex = GetPedBoneIndex(playerPed, 18905)
 			AttachEntityToEntity(prop, playerPed, boneIndex, 0.12, 0.028, 0.001, 10.0, 175.0, 0.0, true, true, false, true, 1, true)
-
 			ESX.Streaming.RequestAnimDict('mp_player_inteat@burger', function()
-				TaskPlayAnim(playerPed, 'mp_player_inteat@burger', 'mp_player_int_eat_burger_fp', 8.0, -8, -1, 49, 0, 0, 0, 0)
-
-				Citizen.Wait(3000)
+			TaskPlayAnim(playerPed, 'mp_player_inteat@burger', 'mp_player_int_eat_burger_fp', 8.0, -8, -1, 49, 0, 0, 0, 0)
+				Wait(3000)
 				IsAnimated = false
 				ClearPedSecondaryTask(playerPed)
 				DeleteObject(prop)
 			end)
 		end)
-
 	end
 end)
 
@@ -122,22 +111,19 @@ AddEventHandler('esx_basicneeds:onDrink', function(prop_name)
 		prop_name = prop_name or 'prop_ld_flow_bottle'
 		IsAnimated = true
 
-		Citizen.CreateThread(function()
+		CreateThread(function()
 			local playerPed = PlayerPedId()
 			local x,y,z = table.unpack(GetEntityCoords(playerPed))
 			local prop = CreateObject(GetHashKey(prop_name), x, y, z + 0.2, true, true, true)
 			local boneIndex = GetPedBoneIndex(playerPed, 18905)
 			AttachEntityToEntity(prop, playerPed, boneIndex, 0.12, 0.028, 0.001, 10.0, 175.0, 0.0, true, true, false, true, 1, true)
-
 			ESX.Streaming.RequestAnimDict('mp_player_intdrink', function()
-				TaskPlayAnim(playerPed, 'mp_player_intdrink', 'loop_bottle', 1.0, -1.0, 2000, 0, 1, true, true, true)
-
-				Citizen.Wait(3000)
+			TaskPlayAnim(playerPed, 'mp_player_intdrink', 'loop_bottle', 1.0, -1.0, 2000, 0, 1, true, true, true)
+				Wait(3000)
 				IsAnimated = false
 				ClearPedSecondaryTask(playerPed)
 				DeleteObject(prop)
 			end)
 		end)
-
 	end
 end)

42d8be597b836ef17bc35f5881fc42bb5bd127d2
SyncEye <50116007+DamjanPetrovicki@users.noreply.github.com>
2020-10-11 12:41:52 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -22,7 +22,7 @@ end)
 
 ESX.RegisterCommand('heal', 'admin', function(xPlayer, args, showError)
 	args.playerId.triggerEvent('esx_basicneeds:healPlayer')
-	args.playerId.triggerEvent('chat:addMessage', {args = {'^3Sync Installer', ' You have been healed by ' .. xPlayer.getName()}})
+	args.playerId.triggerEvent('chat:addMessage', {args = {'^3Heal ^5System', ' You have been healed by ' .. xPlayer.getName()}})
 end, true, {help = 'Heal a player, or yourself - restores thirst, hunger and health.', validate = true, arguments = {
 	{name = 'playerId', help = 'the player id', type = 'player'}
 }})
